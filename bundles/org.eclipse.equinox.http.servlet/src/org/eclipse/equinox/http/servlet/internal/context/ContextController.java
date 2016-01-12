adf5668bf2d4a5535e1eff9baf4267b54f3edf42
shuyangzhou <shuyang.zhou@liferay.com>
2016-01-12 10:55:46 -0800

    Proper concurrency handling

--- bundles/org.eclipse.equinox.http.servlet/src/org/eclipse/equinox/http/servlet/internal/context/ContextController.java
+++ bundles/org.eclipse.equinox.http.servlet/src/org/eclipse/equinox/http/servlet/internal/context/ContextController.java
@@ -15,6 +15,8 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.security.AccessController;
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.regex.Pattern;
@@ -1136,20 +1138,13 @@ public class ContextController {
 	}
 
 	private void flushActiveSessions() {
-		Collection<HttpSessionAdaptor> currentActiveSessions;
-		synchronized (activeSessions) {
-			currentActiveSessions = new ArrayList<HttpSessionAdaptor>(activeSessions.values());
-			activeSessions.clear();
-		}
-		for (HttpSessionAdaptor httpSessionAdaptor : currentActiveSessions) {
+		for (HttpSessionAdaptor httpSessionAdaptor : activeSessions.values()) {
 			httpSessionAdaptor.invalidate();
 		}
 	}
 
 	public void removeActiveSession(HttpSession session) {
-		synchronized (activeSessions) {
-			activeSessions.remove(session);
-		}
+		activeSessions.remove(session);
 	}
 
 	public void fireSessionIdChanged(String oldSessionId) {
@@ -1164,11 +1159,7 @@ public class ContextController {
 			return;
 		}
 
-		Collection<HttpSessionAdaptor> currentActiveSessions;
-		synchronized (activeSessions) {
-			currentActiveSessions = new ArrayList<HttpSessionAdaptor>(activeSessions.values());
-		}
-		for (HttpSessionAdaptor httpSessionAdaptor : currentActiveSessions) {
+		for (HttpSessionAdaptor httpSessionAdaptor : activeSessions.values()) {
 			HttpSessionEvent httpSessionEvent = new HttpSessionEvent(httpSessionAdaptor);
 			for (javax.servlet.http.HttpSessionIdListener listener : listeners) {
 				listener.sessionIdChanged(httpSessionEvent, oldSessionId);
@@ -1178,22 +1169,30 @@ public class ContextController {
 
 	public HttpSessionAdaptor getSessionAdaptor(
 		HttpSession session, ServletContext servletContext) {
-		boolean created = false;
-		HttpSessionAdaptor sessionAdaptor;
-		synchronized (activeSessions) {
-			sessionAdaptor = activeSessions.get(session);
-			if (sessionAdaptor == null) {
-				created = true;
-				sessionAdaptor = HttpSessionAdaptor.createHttpSessionAdaptor(session, servletContext, this);
-				activeSessions.put(session, sessionAdaptor);
-			}
+
+		HttpSessionAdaptor httpSessionAdaptor = activeSessions.get(session);
+
+		if (httpSessionAdaptor != null) {
+			return httpSessionAdaptor;
 		}
-		if (created) {
-			for (HttpSessionListener listener : eventListeners.get(HttpSessionListener.class)) {
-				listener.sessionCreated(new HttpSessionEvent(sessionAdaptor));
-			}
+
+		httpSessionAdaptor = HttpSessionAdaptor.createHttpSessionAdaptor(
+			session, servletContext, this);
+
+		HttpSessionAdaptor previousHttpSessionAdaptor =
+			activeSessions.putIfAbsent(session, httpSessionAdaptor);
+
+		if (previousHttpSessionAdaptor != null) {
+			return previousHttpSessionAdaptor;
 		}
-		return sessionAdaptor;
+
+		for (HttpSessionListener listener : eventListeners.get(
+				HttpSessionListener.class)) {
+
+			listener.sessionCreated(new HttpSessionEvent(httpSessionAdaptor));
+		}
+
+		return httpSessionAdaptor;
 	}
 
 	private void validate(String preValidationContextName, String preValidationContextPath) {
@@ -1228,7 +1227,7 @@ public class ContextController {
 	private final Set<EndpointRegistration<?>> endpointRegistrations = new ConcurrentSkipListSet<EndpointRegistration<?>>();
 	private final EventListeners eventListeners = new EventListeners();
 	private final Set<FilterRegistration> filterRegistrations = new ConcurrentSkipListSet<FilterRegistration>();
-	private final Map<HttpSession, HttpSessionAdaptor> activeSessions = new HashMap<HttpSession, HttpSessionAdaptor>();
+	private final ConcurrentMap<HttpSession, HttpSessionAdaptor> activeSessions = new ConcurrentHashMap<HttpSession, HttpSessionAdaptor>();
 
 	private final HttpServiceRuntimeImpl httpServiceRuntime;
 	private final Set<ListenerRegistration> listenerRegistrations = new HashSet<ListenerRegistration>();
